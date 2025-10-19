606ad036a2351cb3bfdd12f91f3e1b7a1824cc25
umar009-farooq <rusumumarfarooq@gmail.com>
2025-10-19 09:37:59 +0530

    Add intentional failure test for CI pipeline
    Added a test that intentionally fails to verify CI pipeline.

--- src/test/java/com/example/product_service/ProductServiceApplicationTests.java
+++ src/test/java/com/example/product_service/ProductServiceApplicationTests.java
@@ -2,12 +2,16 @@ package com.example.product_service;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import static org.junit.jupiter.api.Assertions.fail;
 
 @SpringBootTest
 class ProductServiceApplicationTests {
 
 	@Test
-	void contextLoads() {
-	}
+void thisTestWillIntentionallyFail() {
+    // This 'fail()' method from JUnit will
+    // automatically fail the test.
+    fail("Deliberate failure to test the CI pipeline!");
+}
 
 }
